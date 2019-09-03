67001c24faed25fccfd5c0d425117ca399faf1ca
Ahrav Dutta <ahravdutta02@gmail.com>
2019-09-02 20:36:09 -0700

    add image carousel to home page

--- BlogApp/blogsite/home/models.py
+++ BlogApp/blogsite/home/models.py
@@ -1,11 +1,30 @@
 from django.db import models
+
+from modelcluster.fields import ParentalKey
+
 from wagtail.admin.edit_handlers import (FieldPanel, PageChooserPanel,
-                                         StreamFieldPanel)
+                                         StreamFieldPanel, InlinePanel,
+                                         MultiFieldPanel)
 from wagtail.core.fields import RichTextField, StreamField
-from wagtail.core.models import Page
+from wagtail.core.models import Page, Orderable
 from wagtail.images.edit_handlers import ImageChooserPanel
 
 from streams import blocks
+
+
+class HomePageCarouselImages(Orderable):
+    """Between 1-5 images for home page carousel"""
+
+    page = ParentalKey('home.HomePage', related_name='carousel_images')
+    carousel_image = models.ForeignKey('wagtailimages.Image',
+                                       null=True,
+                                       blank=False,
+                                       on_delete=models.SET_NULL,
+                                       related_name='+')
+
+    panels = [
+        ImageChooserPanel('carousel_image'),
+    ]
 
 
 class HomePage(Page):
@@ -36,10 +55,18 @@ class HomePage(Page):
     )
 
     content_panels = Page.content_panels + [
-        FieldPanel('banner_title'),
-        FieldPanel('banner_subtitle'),
-        ImageChooserPanel('banner_image'),
-        PageChooserPanel('banner_cta'),
+        MultiFieldPanel([
+            FieldPanel('banner_title'),
+            FieldPanel('banner_subtitle'),
+            ImageChooserPanel('banner_image'),
+            PageChooserPanel('banner_cta'),
+        ],
+                        heading='Banner Options'),
+        MultiFieldPanel([
+            InlinePanel('carousel_images', max_num=5, min_num=1,
+                        label='Image'),
+        ],
+                        heading='Carousel Images'),
         StreamFieldPanel('content'),
     ]
 
