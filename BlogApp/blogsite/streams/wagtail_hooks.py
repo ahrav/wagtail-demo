83afcb389173277a6b0a3bd527a3d1c5fb0b1fd1
Ahrav Dutta <ahravdutta02@gmail.com>
2019-09-04 00:41:34 -0700

    Add custom richtext editor fields including code block and center code button

---
+++ BlogApp/blogsite/streams/wagtail_hooks.py
@@ -0,0 +1,72 @@
+from wagtail.core import hooks
+import wagtail.admin.rich_text.editors.draftail.features as draftail_features
+from wagtail.admin.rich_text.converters.html_to_contentstate import InlineStyleElementHandler
+
+
+@hooks.register('register_rich_text_features')
+def register_code_styling(features):
+    """Add the <code> to the richtext editor."""
+
+    feature_name = 'code'
+    type_ = 'CODE'
+    tag = 'code'
+
+    control = {
+        'type': type_,
+        'label': '</>',
+        'description': 'Code'
+    }
+
+    features.register_editor_plugin(
+        'draftail', feature_name, draftail_features.InlineStyleFeature(control)
+    )
+
+    db_conversion = {
+        'from_database_format': {tag: InlineStyleElementHandler(type_)},
+        'to_databse_format': {'style_map': {type_: {'element': tag}}}
+    }
+
+    features.register_converter_rule('contentstate', feature_name, db_conversion)
+
+    # register feature to all richtext editors by default
+    features.default_features.append(feature_name)
+
+@hooks.register('register_rich_text_features')
+def register_centertext_feature(features):
+    """Create center text in richtext."""
+
+    feature_name = 'center'
+    type_ = 'CENTERTEXT'
+    tag = 'div'
+
+    control = {
+        'type': type_,
+        'label': 'Center',
+        'description': 'Center Text',
+        'style': {
+            'display': 'block',
+            'text-align': 'center',
+        },
+    }
+
+    features.register_editor_plugin(
+        'draftail', feature_name, draftail_features.InlineStyleFeature(control)
+    )
+
+    db_conversion = {
+        'from_database_format': {tag: InlineStyleElementHandler(type_)},
+        'to_database_format': {
+            'style_map': {
+                type_: {
+                    'element': tag,
+                    'props': {
+                        'class': 'd-block text-center'
+                    }
+                }
+            }
+        }
+    }
+
+    features.register_converter_rule('contentstate', feature_name, db_conversion)
+
+    features.default_features.append(feature_name)
