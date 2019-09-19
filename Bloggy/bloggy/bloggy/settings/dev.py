543323cb169180f184f0779255ac28de09d82c84
Ahrav Dutta <ahravdutta02@gmail.com>
2019-09-18 21:16:12 -0700

    add boostrap theme

--- Bloggy/bloggy/bloggy/settings/dev.py
+++ Bloggy/bloggy/bloggy/settings/dev.py
@@ -7,10 +7,19 @@ DEBUG = True
 SECRET_KEY = 'l4ved$-n9bqoq_7ecgdw4-376(a2!2u*gcx^-=#49v@f1g+phv'
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+INSTALLED_APPS = INSTALLED_APPS + [
+    'debug_toolbar',
+]
+
+MIDDLEWARE = MIDDLEWARE + [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = ('127.0.0.1', )
 
 try:
     from .local import *
