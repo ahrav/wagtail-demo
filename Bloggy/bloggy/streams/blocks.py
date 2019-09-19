ab800d74caa55f80a410da61974ef8917e47e277
Ahrav Dutta <ahravdutta02@gmail.com>
2019-09-18 23:34:29 -0700

    add list blocks, with team members

--- Bloggy/bloggy/streams/blocks.py
+++ Bloggy/bloggy/streams/blocks.py
@@ -1,6 +1,7 @@
 """Streamfields are here"""
 
 from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
 
 
 class TitleAndTextBlock(blocks.StructBlock):
@@ -13,6 +14,26 @@ class TitleAndTextBlock(blocks.StructBlock):
         template = 'streams/title_and_text_block.html'
         icon = 'edit'
         label = 'Title & Text'
+
+
+class CardBlock(blocks.StructBlock):
+    """Cards with image and text and button(s)"""
+
+    title = blocks.CharBlock(required=True, help_text='Add your title')
+
+    cards = blocks.ListBlock(
+        blocks.StructBlock([
+            ('image', ImageChooserBlock(required=True)),
+            ('name', blocks.CharBlock(required=True, max_length=40)),
+            ('title', blocks.TextBlock(required=True, max_length=200)),
+            ('button_page', blocks.PageChooserBlock(required=False)),
+            ('button_url', blocks.URLBlock(required=False, help_text='If button page above selected, that will be used first')),
+        ]))
+
+    class Meta:  #noqa
+        template = 'streams/card_block.html'
+        icon = 'placeholder'
+        label = 'Staff Cards'
 
 
 class RichTextBlock(blocks.RichTextBlock):
