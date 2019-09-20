97165e78f2ba343f37e142a4dfe509e64435caa9
Ahrav Dutta <ahravdutta02@gmail.com>
2019-09-19 23:20:46 -0700

    add latest blog posts page

--- Bloggy/bloggy/blog/models.py
+++ Bloggy/bloggy/blog/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from django.shortcuts import render
 from wagtail.core.models import Page
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 from wagtail.core.fields import StreamField, RichTextField
 from wagtail.images.edit_handlers import ImageChooserPanel
 
 from streams import blocks
 
 
-class BlogListingPage(Page):
+class BlogListingPage(RoutablePageMixin, Page):
     """Page that lists all blog objects"""
 
     template = 'blog/blog_listing_page.html'
@@ -27,6 +29,12 @@ class BlogListingPage(Page):
         context = super().get_context(request, *args, **kwargs)
         context['posts'] = BlogDetailPage.objects.live().public()
         return context
+
+    @route(r'^latest/$')
+    def latest_blog_post(self, request, *args, **kwargs):
+        context = self.get_context(request, *args, **kwargs)
+        context['posts'] = context['posts'][:1]
+        return render(request, 'blog/latest_posts.html', context)
 
 
 class BlogDetailPage(Page):
